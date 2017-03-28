0a7e860828e8c44898b8d15fdb31bf26ee71e6a9
Hossein Shafiei <hs.shafiei@gmail.com>
2017-03-28 20:32:47 +0430

    crawler fixed

--- crawler.js
+++ crawler.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'salam64511',
-  database : 'shoa'
+  database : 'shoa2'
 });
 
 var d = new Crawler({
