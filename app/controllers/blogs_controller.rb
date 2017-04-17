96e0ebabeb59ecbb57b569f0eccf471f8570481a
Hossein Shafiei <hs.shafiei@gmail.com>
2017-04-17 22:14:04 +0430

    bug fixes 1

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -69,7 +69,7 @@ class BlogsController < ApplicationController
   def update
     respond_to do |format|
       if @blog.update(blog_params)
-        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
+        format.html {  redirect_to '/blogs?blog_id='+@blog.id.to_s, notice: 'Blog was successfully updated.' }
         format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit }
