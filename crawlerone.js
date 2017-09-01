2708510b054025fb773fe451c4635b9ee055f436
Hossein Shafiei <hs.shafiei@gmail.com>
2017-09-01 17:20:55 +0430

    login improved 6

--- crawlerone.js
+++ crawlerone.js
@@ -78,10 +78,7 @@ var researches = [];
 var sql = 'SELECT * FROM researches where utid =' + mysql.escape(utid);
 connection.query(sql, function (error, rows) {
   if (error) throw error;
-  for(r in rows){
-    console.log(rows[r].RowDataPacket);
-    console.log('-----');
-  }
+  console.log(rows);
 //  console.log(results);
 });
 });
