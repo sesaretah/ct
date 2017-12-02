18679bb7a11cca1032cd76f2769912ae19c142ea
Hossein Shafiei <hs.shafiei@gmail.com>
2017-12-02 09:37:58 +0330

    updated 88

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'sn.ut.ac.ir', port: 80 }
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'utservm.ut.ac.ir',
